6592677a2133c43652ef23bf5c2fe063246d22aa
Prxyankaz <137303413+Prxyankaz@users.noreply.github.com>
2025-02-24 18:20:17 +0530

    user.js updated

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const mongoose = require("mongoose");
+
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true }, // ✅ Match the request body
